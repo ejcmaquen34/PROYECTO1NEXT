75b0537c7feb71f8d6f7ecbcfee4c96a1df05d7b
unknown <ejcmaquen34@gmail.com>
2026-04-23 22:16:52 -0500

    lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,6 @@
-import { Inter } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'], // Specify the weights you need
+  subsets: ['latin'],     // Define character subsets
+});
